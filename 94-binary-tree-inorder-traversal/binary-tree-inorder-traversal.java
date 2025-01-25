545f3053e22e286253e5ab6ed52c33b8cf0ab017
Sudharani Bannengala <sudharanib27@gmail.com>
2025-01-25 13:28:50 -0600

    Time: 0 ms (100.00%) | Memory: 41.7 MB (43.92%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
@@ -13,13 +13,46 @@
  *     }
  * }
  */
+// class Solution {
+//     List<Integer> output = new ArrayList<>();
+//     public List<Integer> inorderTraversal(TreeNode root) {
+//         if (root == null) return output;
+//         inorderTraversal(root.left);
+//         output.add(root.val);
+//         inorderTraversal(root.right);
+//         return output;
+//     }
+// }
+
+// Morris Traversal
 class Solution {
     List<Integer> output = new ArrayList<>();
     public List<Integer> inorderTraversal(TreeNode root) {
-        if (root == null) return output;
-        inorderTraversal(root.left);
-        output.add(root.val);
-        inorderTraversal(root.right);
+        TreeNode cur = root;
+        while (cur != null) {
+            if (cur.left == null) {
+                output.add(cur.val);
+                cur = cur.right;
+            }
+            else {
+                TreeNode prev = cur.left;
+                while (prev.right != null && prev.right != cur) {
+                    prev = prev.right;
+                }
+
+                if (prev.right == null) {
+                    prev.right = cur;
+                    cur = cur.left;
+                }
+
+                else {
+                    prev.right = null;
+                    output.add(cur.val);
+                    cur = cur.right;
+                }
+            }
+        }
+
         return output;
     }
 }
