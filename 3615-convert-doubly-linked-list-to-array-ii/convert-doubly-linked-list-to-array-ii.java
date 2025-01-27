4a85f766239e7823b787bfb78d5fcc5f771148b9
Sudharani Bannengala <sudharanib27@gmail.com>
2025-01-27 14:28:49 -0600

    Time: 1 ms (100.00%) | Memory: 45.8 MB (24.47%) - LeetSync

--- 3615-convert-doubly-linked-list-to-array-ii/convert-doubly-linked-list-to-array-ii.java
+++ 3615-convert-doubly-linked-list-to-array-ii/convert-doubly-linked-list-to-array-ii.java
@@ -7,31 +7,57 @@ class Node {
 };
 */
 
+// class Solution {
+//     public int[] toArray(Node node) {
+//         List<Integer> resultList = new ArrayList<>();
+
+//         // copy right side of the given node
+//         Node right = node;
+//         while (right != null) {
+//             resultList.add(right.val);
+//             right = right.next;
+//         }
+
+//         // Copy left side of the given node starting from index 0
+//         Node left = node.prev;
+//         while (left != null) {
+//             resultList.add(0, left.val);
+//             left = left.prev;
+//         }
+
+//         // System.out.println(resultList);
+//         int[] result_final = new int[resultList.size()];
+
+//         for (int i=0; i < resultList.size(); ++i) {
+//             result_final[i] = resultList.get(i);
+//         }
+//         return result_final;
+
+//     }
+// }
+
+//Approach 3:
 class Solution {
     public int[] toArray(Node node) {
-        List<Integer> resultList = new ArrayList<>();
-
-        // copy right side of the given node
-        Node right = node;
-        while (right != null) {
-            resultList.add(right.val);
-            right = right.next;
+        Node cur = node;
+        while (cur.prev != null) {
+            cur = cur.prev;
         }
 
-        // Copy left side of the given node starting from index 0
-        Node left = node.prev;
-        while (left != null) {
-            resultList.add(0, left.val);
-            left = left.prev;
+        Node head = cur;
+        int n = 0;
+        while (cur != null) {
+            n++;
+            cur = cur.next;
         }
 
-        // System.out.println(resultList);
-        int[] result_final = new int[resultList.size()];
+        int[] result = new int[n];
 
-        for (int i=0; i < resultList.size(); ++i) {
-            result_final[i] = resultList.get(i);
+        for (int i = 0; i < n; ++i) {
+            result[i] = head.val;
+            head = head.next;
         }
-        return result_final;
 
+        return result;
     }
 }
