ef609cf23efb536b15e28eb65b01e9c4f4ecc948
Sudharani Bannengala <sudharanib27@gmail.com>
2025-01-29 15:24:02 -0600

    Time: 15 ms (95.14%) | Memory: 112.3 MB (98.08%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
@@ -1,36 +1,37 @@
-class Solution {
-    public boolean validPath(int n, int[][] edges, int source, int destination) {
-        Map<Integer, List<Integer>> graph = new HashMap<>();
-        Set<Integer> visited = new HashSet<>();
+// Approach 1: DFS
+// class Solution {
+//     public boolean validPath(int n, int[][] edges, int source, int destination) {
+//         Map<Integer, List<Integer>> graph = new HashMap<>();
+//         Set<Integer> visited = new HashSet<>();
 
-        for(int[] edge: edges) {
-            graph.computeIfAbsent(edge[0], k -> new ArrayList<>()).add(edge[1]);
-            graph.computeIfAbsent(edge[1], k -> new ArrayList<>()).add(edge[0]);
-        }
+//         for(int[] edge: edges) {
+//             graph.computeIfAbsent(edge[0], k -> new ArrayList<>()).add(edge[1]);
+//             graph.computeIfAbsent(edge[1], k -> new ArrayList<>()).add(edge[0]);
+//         }
 
-        return dfs(source, destination, graph, visited);
+//         return dfs(source, destination, graph, visited);
         
-    }
+//     }
 
-    private boolean dfs(int node, int destination, Map<Integer, List<Integer>> graph, Set<Integer> visited) {
-            if (node == destination) return true;
-            visited.add(node);
-            List<Integer> neighbors = graph.getOrDefault(node, new ArrayList<>());
+//     private boolean dfs(int node, int destination, Map<Integer, List<Integer>> graph, Set<Integer> visited) {
+//             if (node == destination) return true;
+//             visited.add(node);
+//             List<Integer> neighbors = graph.getOrDefault(node, new ArrayList<>());
 
-            for (int neighbor: neighbors) {
-                if (!visited.contains(neighbor)) {
-                    if (dfs(neighbor, destination, graph, visited)) {
-                        return true;
-                    }
-                }
-            }
+//             for (int neighbor: neighbors) {
+//                 if (!visited.contains(neighbor)) {
+//                     if (dfs(neighbor, destination, graph, visited)) {
+//                         return true;
+//                     }
+//                 }
+//             }
 
-            return false;
-    }
-}
+//             return false;
+//     }
+// }
 
 
-// BFS
+// Approach 2: BFS
 // class Solution {
 //     public boolean validPath(int n, int[][] edges, int source, int destination) {
 //         /* Define HashMap to to store the adjacency list */
@@ -63,3 +64,46 @@ class Solution {
 //         return false;
 //     }
 // }
+
+// Approach 3:  Union Find
+class Solution {
+    private int[] parent;
+    private int[] rank;
+    public boolean validPath(int n, int[][] edges, int source, int destination) {
+        parent = new int[n];
+        rank = new int[n];
+        for (int i=0; i<n; ++i) {
+            parent[i] = i;
+            rank[i] = 1;
+        }
+
+        for (int[] edge: edges) {
+            union(edge[0], edge[1]);
+        }
+
+        return find(source) == find(destination);
+    }
+
+    private int find(int x) {
+        if (parent[x] != x) {
+            parent[x] = find(parent[x]);
+        }
+        return parent[x];
+    }
+
+    private void union(int x, int y) {
+        int xroot = find(x);
+        int yroot = find(y);
+
+        if (rank[xroot] > rank[yroot]) {
+            parent[yroot] = xroot;
+        }
+        else if(rank[xroot] < rank[yroot]) {
+            parent[xroot] = yroot;
+        }
+        else {
+            parent[yroot] = xroot;
+            rank[xroot]++;
+        }
+    }
+}
