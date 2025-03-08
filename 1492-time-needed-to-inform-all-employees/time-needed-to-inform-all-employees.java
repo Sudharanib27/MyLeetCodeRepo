0fea7ca7d55881a0f350446af4ece3777de0a3e3
Sudharani Bannengala <sudharanib27@gmail.com>
2025-03-08 11:28:50 -0800

    Time: 183 ms (6.33%) | Memory: 65.6 MB (29.08%) - LeetSync

---
+++ 1492-time-needed-to-inform-all-employees/time-needed-to-inform-all-employees.java
@@ -0,0 +1,30 @@
+class Solution {
+    Map<Integer, List<Integer>> graph;
+    public int numOfMinutes(int n, int headID, int[] manager, int[] informTime) {
+        graph = new HashMap<>();
+        
+        for (int i=0; i<n; i++) {
+            List<Integer> list = graph.getOrDefault(manager[i], new ArrayList<>());
+            list.add(i);
+            graph.put(manager[i], list);
+        }
+
+        System.out.println(graph);
+
+        return dfs(headID, informTime);
+    }
+
+    int dfs(int headID, int[] informTime) {
+
+        int max = 0;
+        if (!graph.containsKey(headID)) {
+            return max;
+        }
+
+        for (int subordinate : graph.get(headID)) {
+            max = Math.max(max, dfs(subordinate, informTime));
+        }
+
+        return max + informTime[headID];
+    }
+}
