a7eadda5437e1d6c9fa29c314ff993802b954c6f
Sudharani Bannengala <sudharanib27@gmail.com>
2025-04-02 08:30:27 -0700

    Time: 2 ms (84.98%) | Memory: 45.5 MB (98.98%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.java
@@ -0,0 +1,21 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int L = 0;
+        int R = 1;
+
+        while (R < nums.length) {
+            if (nums[L] != 0) {
+                L++;
+                R++;
+            }
+            else if (nums[R] == 0) {
+                R++;
+            }
+            else {
+                int temp = nums[L];
+                nums[L] = nums[R];
+                nums[R] = temp;
+            }
+        }
+    }
+}
