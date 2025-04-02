b5e9574996da416313f9dd37535b79398fa55e59
Sudharani Bannengala <sudharanib27@gmail.com>
2025-04-02 08:38:33 -0700

    Time: 2 ms (84.98%) | Memory: 46 MB (64.58%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,36 +1,36 @@
 class Solution {
     public void moveZeroes(int[] nums) {
         // Method 1:
-        // int L = 0;
-        // int R = 1;
+        int L = 0;
+        int R = 1;
 
-        // while (R < nums.length) {
-        //     if (nums[L] != 0) {
-        //         L++;
-        //         R++;
-        //     }
-        //     else if (nums[R] == 0) {
-        //         R++;
-        //     }
-        //     else {
-        //         int temp = nums[L];
-        //         nums[L] = nums[R];
-        //         nums[R] = temp;
-        //     }
-        // }
-
-        // Method 2:
-        int r = 0;
-        for (int l = 0; l < nums.length; l++) {
-            if (nums[l] != 0 && nums[r] == 0) {
-                int temp = nums[l];
-                nums[l] = nums[r];
-                nums[r] = temp;
+        while (R < nums.length) {
+            if (nums[L] != 0) {
+                L++;
+                R++;
             }
-
-            if (nums[r] != 0) {
-                r++;
+            else if (nums[R] == 0) {
+                R++;
+            }
+            else {
+                int temp = nums[L];
+                nums[L] = nums[R];
+                nums[R] = temp;
             }
         }
+
+        // Method 2:
+        // int r = 0;
+        // for (int l = 0; l < nums.length; l++) {
+        //     if (nums[l] != 0 && nums[r] == 0) {
+        //         int temp = nums[l];
+        //         nums[l] = nums[r];
+        //         nums[r] = temp;
+        //     }
+
+        //     if (nums[r] != 0) {
+        //         r++;
+        //     }
+        // }
     }
 }
