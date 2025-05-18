d5ef51d159672c57b275562f400f78e37d2b686b
Sudharani Bannengala <sudharanib27@gmail.com>
2025-05-17 18:32:11 -0700

    Time: 35 ms (81.97%) | Memory: 17.8 MB (56.95%) - LeetSync

--- 7-reverse-integer/reverse-integer.py
+++ 7-reverse-integer/reverse-integer.py
@@ -1,5 +1,20 @@
 class Solution:
     def reverse(self, x: int) -> int:
-        sign = -1 if x < 0 else 1
-        res = int(str(abs(x)) [::-1]) * sign
-        return res if -(2**31) <= res <= (2**31) else 0
+        reb_flag = False
+        if x < 0:
+            x = 0 - x
+            reb_flag = True
+            biggest = str(2**31)
+        else:
+            biggest = str((2**31)-1)
+        
+        x_str = str(x)[::-1]
+        
+        if len(x_str) >= len(biggest):
+            for i in range(len(x_str)):
+                if x_str[i] > biggest[i]:
+                    return 0
+                if x_str[i] < biggest[i]:
+                    break
+
+        return int(x_str) if not reb_flag else 0-int(x_str)
