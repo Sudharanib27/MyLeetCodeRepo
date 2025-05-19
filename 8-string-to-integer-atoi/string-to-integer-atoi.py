efc37467cf2508e39f7b444085493609945a2d79
Sudharani Bannengala <sudharanib27@gmail.com>
2025-05-19 15:22:43 -0700

    Time: 2 ms (46.90%) | Memory: 18.1 MB (8.72%) - LeetSync

---
+++ 8-string-to-integer-atoi/string-to-integer-atoi.py
@@ -0,0 +1,29 @@
+class Solution:
+    def myAtoi(self, s: str) -> int:
+        # trim the space
+        s = s.strip()
+
+        if not s:
+            return 0
+
+        i, sign, res = 0, 1, 0
+
+        # Checking for the sign
+        if s[0] == '-':
+            sign = -1
+            i += 1
+
+        elif s[0] == '+':
+            i += 1
+
+        while i < len(s) and s[i].isdigit():
+            res = res * 10 + int(s[i])
+
+            if sign * res > 2**31 - 1:
+                return 2**31 - 1
+            if sign * res < -2**31:
+                return -2**31
+            
+            i += 1
+        
+        return sign * res
