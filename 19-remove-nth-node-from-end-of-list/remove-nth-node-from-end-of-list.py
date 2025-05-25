563c09c7414f3a7aa1259a928a0a009818f0565a
Sudharani Bannengala <sudharanib27@gmail.com>
2025-05-25 16:12:20 -0700

    Time: 0 ms (100.00%) | Memory: 17.6 MB (84.50%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
@@ -5,25 +5,41 @@
 #         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        cur = head
-        count = 0
+        ### 1. Two pass Bruteforce
+        # cur = head
+        # count = 0
 
-        # Find the length of the list
-        while cur:
-            cur = cur.next
-            count += 1
-        print(count)
+        # # Find the length of the list
+        # while cur:
+        #     cur = cur.next
+        #     count += 1
+        # print(count)
 
-        # edge case if count == n
-        if count == n:
-            return head.next
+        # # edge case if count == n
+        # if count == n:
+        #     return head.next
 
-        # get cur to the right position
-        cur = head
-        i = 0
-        while i < count-n-1:
-            cur = cur.next
-            i += 1
+        # # get cur to the right position
+        # cur = head
+        # i = 0
+        # while i < count-n-1:
+        #     cur = cur.next
+        #     i += 1
         
-        cur.next = cur.next.next
+        # cur.next = cur.next.next
+        # return head
+
+        ### 2. Optimised single pass
+        slow = head
+        fast = head
+        count = 0
+        while count < n and fast:
+            fast = fast.next
+            count += 1
+        if not fast:
+            return head.next
+        while fast.next:
+            slow = slow.next
+            fast = fast.next
+        slow.next = slow.next.next
         return head
